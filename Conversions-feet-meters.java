7ae86434fdafea9ad89262c178006e15db17e6da
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-09 11:49:38 -0400

    Create Conversions-feet-meters.java

---
+++ Conversions-feet-meters.java
@@ -0,0 +1,8 @@
+// Convert from feet to meters
+public static double footToMeter (double foot)
+
+// Convert from meters to feet
+public static double meterToFoot (double foot)
+
+  meter = 0.305 * foot
+  foot = 3.279 * meter
