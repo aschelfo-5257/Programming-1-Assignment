68db410c1ff25a02d910b947e6f24b36cf8c8509
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-06-09 12:38:43 -0400

    Conversions-feet-meters.java

--- Conversions-feet-meters.java
+++ Conversions-feet-meters.java
@@ -3,15 +3,16 @@ public class Conversions {
   private static final double meters_to_feet
 
 // Convert from feet to meters
-public static double feetToMeter (double feet) {
+public static double feetToMeters (double feet) {
   return feet;
 }
   
 // Convert from meters to feet
-public static double meterToFeet (double feet) {
-  return meter;
+public static double metersToFeet (double meters) {
+  return meters;
 }
 
+// Main method for Conversions
 public static void main(String[] args) {
   double feet = 0.305;
   double meters = feetToMeters(feet);
